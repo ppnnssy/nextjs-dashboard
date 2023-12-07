22daa39758c15578e7649021e6207dd639d1d42b
ZCS <zhouchongshen@163.com>
2023-12-07 14:41:11 +0800

    项目完成

---
+++ app/not-found.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import { FaceFrownIcon } from '@heroicons/react/24/outline';
+
+export default function NotFound() {
+  return (
+    <main className="flex h-full flex-col items-center justify-center gap-2">
+      <Link
+        href="/dashboard/invoices"
+        className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
+      >
+        Go Back
+      </Link>
+    </main>
+  );
+}
